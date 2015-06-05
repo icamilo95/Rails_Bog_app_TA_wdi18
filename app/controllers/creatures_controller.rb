f14b7f0697ab5101d50556fe4a87f2b3e8ddaeca
Camilo Ordonez <icoj95@gmail.com>
2015-06-05 01:02:14 -0700

    CRUD working

--- app/controllers/creatures_controller.rb
+++ app/controllers/creatures_controller.rb
@@ -22,7 +22,25 @@ class CreaturesController < ApplicationController
       render :show
    end
 
+   def edit
+      id = params[:id]
+      @creature = Creature.find(id)
+      render :edit
+   end
 
+   def update
+      creature_id = params[:id]
+      @creature = Creature.find(creature_id)
+      updated_attributes = params.require(:creature).permit(:name, :description)
+      @creature.update_attributes(updated_attributes)
+      redirect_to "/creatures/#{creature_id}"
+   end
 
+   def destroy
+      creature_id = params[:id]
+      creature = Creature.find(creature_id)
+      creature.destroy
+      redirect_to "/creatures"
+   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ Rails.application.routes.draw do
 
   root to: 'creatures#index'
 
-  get '/creatures', to: "creatures#index"
-  get '/creatures/new', to: "creatures#new"
+  get '/creatures', to: "creatures#index", as: "creatures"
+  get '/creatures/new', to: "creatures#new", as: "new_creature"
   post '/creatures', to: "creatures#create"
-  get '/creatures/:id', to: "creatures#show"
-
+  get '/creatures/:id', to: "creatures#show", as: "creature"
+  get '/creatures/:id/edit', to: "creatures#edit", as: "edit_creatures"
+  patch '/creatures/:id', to: "creatures#update"
+  delete '/creatures/:id', to: "creatures#destroy"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
