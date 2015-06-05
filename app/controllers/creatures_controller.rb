65435bd32c31d6f00f69db674f1d328b167ddfc2
Camilo Ordonez <icoj95@gmail.com>
2015-06-05 00:27:08 -0700

    Added CR to the app

---
+++ app/controllers/creatures_controller.rb
@@ -0,0 +1,28 @@
+class CreaturesController < ApplicationController
+
+   def index
+      @creatures = Creature.all
+      render :index
+   end
+
+   def new  
+      @creature = Creature.new
+      render :new
+   end
+
+   def create
+      new_creature = params.require(:creature).permit(:name, :description)
+      creature = Creature.create(new_creature)
+      redirect_to "/creatures/#{creature.id}"
+   end
+
+   def show
+      id = params[:id]
+      @creature = Creature.find(id)
+      render :show
+   end
+
+
+
+
+end
